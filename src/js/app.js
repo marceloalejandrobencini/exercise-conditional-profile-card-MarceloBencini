aaf51f13569ffc611c3858525aeec1cc85065df6
Marcelo Bencini <140016015+marceloalejandrobencini@users.noreply.github.com>
2024-01-27 00:19:15 +0000

    ptoyecto finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,13 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
-        
+
         twitter: null, // social media usernames
         github: null,
         linkedin: null,
@@ -26,24 +26,54 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  // generar el cover
+  let coverHTML = variables.includeCover
+    ? `<div class="cover"><img src="${variables.background}" /></div>`
+    : "<div class='cover'></div>";
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  // posicion de los iconos de social media
+  const socialMediaPositionClass =
+    variables.socialMediaPosition === "left"
+      ? "position-left"
+      : "position-right";
+
+  // generar los links de social media solo si tiene input
+  const socialMediaLinksHTML = `
+ <ul class="${socialMediaPositionClass}">
+   ${
+     variables.twitter
+       ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+       : ""
+   }
+   ${
+     variables.github
+       ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+       : ""
+   }
+   ${
+     variables.linkedin
+       ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+       : ""
+   }
+   ${
+     variables.instagram
+       ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+       : ""
+   }
+ </ul>
+`;
+
+  // actualizar el html
+  document.querySelector("#widget_content").innerHTML = `
+ <div class="widget">
+   ${coverHTML}
+   <img src="${variables.avatarURL}" class="photo" />
+   <h1>${variables.name || "John"} ${variables.lastName || "Doe"}</h1>
+   <h2>${variables.role || "Web Developer"}</h2>
+   <h3>${variables.city || ""}, ${variables.country || ""}</h3>
+   ${socialMediaLinksHTML}
+ </div>
+`;
 }
 
 /**
